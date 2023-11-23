d2b9a8cbb9a1b2cc51d130ac08ac1e2627dcb9cc
jiny <jiny3360@naver.com>
2023-11-23 09:45:49 +0900

    2023_11_23 - #1 init dnd process(issue AppWrite Gen Error)

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Header from "@/components/header";
+import Board from "@/components/board";
 
 export default function Home() {
   return (
     <main>
       <Header />
+      <Board />
     </main>
   );
 }
---
+++ appwrite.ts
@@ -0,0 +1,12 @@
+import { Client, Account, Databases, Storage, ID } from "appwrite";
+
+export const client = new Client();
+
+client
+  .setEndpoint("https://cloud.appwrite.io/v1")
+  .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID!); // Replace with your project ID
+
+export const account = new Account(client);
+export const database = new Databases(client);
+export const storage = new Storage(client);
+export { ID } from "appwrite";
